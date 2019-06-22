ed3c65286635911331c2458e037862cb4c53eb74
weijer <chengwei5566>
2019-06-22 12:51:50 +0800

    增量更新脚本复制文件判断是否有更新

--- git_incremental_update.sh
+++ git_incremental_update.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-# 获取去除tag其他标识函数
-function get_tag_real_name(){
-   tag_real_tag_arr=(`echo $1 | tr '-' ' '`)
-   len=${#tag_real_tag_arr[*]}
-   if (($len>=2)); then
-	 echo ${tag_real_tag_arr[@]:0:$len-2}
-   else
-	 echo $1
-   fi
-}
-
 # 创建更新目录
 rm -rf update_dir
 mkdir update_dir
 
 # 获取最新得两个tag
-last_two_tags=($(git describe --tags `git rev-list --tags --max-count=2`))
+# last_two_tags=($(git describe --tags `git rev-list --tags --max-count=2`))
+last_two_tags=($(git tag -l))
+last_two_tags_len=${#last_two_tags[*]}
+
 
 # 获取去除tag其他标识
-first_tag=${last_two_tags[0]}
-second_tag=${last_two_tags[1]}
-
-first_real_tag=$(get_tag_real_name $first_tag)
-second_real_tag=$(get_tag_real_name $second_tag)
+first_real_tag=${last_two_tags[$last_two_tags_len-1]}
+second_real_tag=${last_two_tags[$last_two_tags_len-2]}
 
 echo "----------------当前对比两个版本-----------------"
 echo $first_real_tag
@@ -42,5 +31,19 @@ echo $item
 echo "rm -rf ${item}" >> update_dir/jenkins_update.sh
 done
 
+# 把重命名的文件也加入删除shell脚本
+rename_file_list=($(git diff $first_real_tag $second_real_tag --name-only --diff-filter=R))
+echo "----------------把重命名的文件也加入删除shell脚本-----------------"
+for item in ${rename_file_list[@]};
+do
+echo $item
+echo "rm -rf ${item}" >> update_dir/jenkins_update.sh
+done
+
+echo "----------------拷贝要更新的文件到update_dir文件夹-----------------"
 # 复制当前版本差异到 update_dir 限制 Add Create Modify Rename 操作
-cp -pv --parents $(git diff $second_real_tag $first_real_tag --name-only --diff-filter=ACMR)  ./update_dir/
+new_file=($(git diff $second_real_tag $first_real_tag --name-only --diff-filter=ACMR))
+new_file_len=${#new_file[*]}
+if (($new_file_len>0)); then
+    cp -pv --parents $(git diff $second_real_tag $first_real_tag --name-only --diff-filter=ACMR)  ./update_dir/
+fi
